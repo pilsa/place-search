bcf5c0a967cb5a46f4357dcc5513181a6f815f73
pilsa115 <pilsa115@naver.com>
2022-02-11 18:43:24 +0900

    couchbase

---
+++ src/main/java/com/pilsa/place/framework/database/CouchbaseConfig.java
@@ -0,0 +1,37 @@
+package com.pilsa.place.framework.database;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
+
+/**
+ * <pre>
+ * 설명 :
+ * </pre>
+ *
+ * @author : pilsa_internet
+ * @since : 2022-02-11 오후 4:10
+ */
+
+@Configuration
+public class CouchbaseConfig extends AbstractCouchbaseConfiguration {
+
+    @Override
+    public String getConnectionString() {
+        return "couchbase://127.0.0.1";
+    }
+
+    @Override
+    public String getUserName() {
+        return "pilsa115";
+    }
+
+    @Override
+    public String getPassword() {
+        return "Fhsivmrkd00@";
+    }
+
+    @Override
+    public String getBucketName() {
+        return "gamesim-sample";
+    }
+}
